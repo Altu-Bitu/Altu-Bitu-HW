0d4b07c44d801abfc53c696d0b4b28193a6b02db
Kyoungwon-Min <76611903+mkwkw@users.noreply.github.com>
2021-09-06 01:04:40 +0900

    [그룹 단어 체커] 9월 6일

--- 9월3일-정렬/1316.cpp
+++ 9월3일-정렬/1316.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int cnt = 0;
+    bool arr[26];
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        fill_n(arr, 26, false);
+        int ch = 0;
+
+        string word;
+        cin >> word;
+
+        arr[word[0] - 'a'] = true;
+        for (int j = 1; j < word.length(); j++) {
+            //cout << word[j] << ' ';
+            if (arr[word[j] - 'a']) {
+                if (word[j - 1] != word[j]) {
+                    //cout << "wrong" << '\n';
+                    break;
+                } else
+                    ch++;
+            } else {
+                arr[word[j] - 'a'] = true;
+                //cout << "good" << '\n';
+                ch++;
+            }
+        }
+
+        if (ch == word.length() - 1)
+            cnt++;
+
+    }
+    cout << cnt;
+
+}
+
 
