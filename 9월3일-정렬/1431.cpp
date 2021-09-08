71302d67a44bed42c704d5bb801b02dd2a250453
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-08 22:38:37 +0900

    [정렬] 9월 8일 -Update

--- 9월3일-정렬/1431.cpp
+++ 9월3일-정렬/1431.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cctype> //isdigit함수 사용하기 위한 헤더
 
 using namespace std;
 
@@ -8,10 +9,19 @@ using namespace std;
 //char ch = '9';
 //cout << ch-'0';  // 9
 
-int sum(string s){
+int sumNum(string s){
     int result = 0;
     for(int i=0; i<s.length(); i++){
-        if (s[i]-'0' >= 0 && s[i]-'0' <= 9) {
+//        if (s[i]-'0' >= 0 && s[i]-'0' <= 9) {
+//            result += s[i]-'0';
+//        }
+
+        //같은 기능 다르게 구현 - isdigit함수 이용
+
+        //isdigit함수: cctype헤더 사용
+        //char형 인자가 숫자이면 0이 아닌 수 반환, 숫자가 아니면 0 qksghks
+        //result에 더할 때는 s[i]-'0'형태로 더하기
+        if(isdigit(s[i])!=0){
             result += s[i]-'0';
         }
     }
@@ -22,8 +32,8 @@ bool cmp(string a, string b){
     if(a.length()!=b.length())
         return a.length()<b.length();
     else {
-        int result1 = sum(a);
-        int result2 = sum(b);
+        int result1 = sumNum(a);
+        int result2 = sumNum(b);
 
         if(result1 != result2){
             return result1<result2;
