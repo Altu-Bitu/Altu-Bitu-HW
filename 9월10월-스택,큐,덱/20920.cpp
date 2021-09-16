646bb315b3807330160b71a210998ce04fecf711
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-16 23:39:30 +0900

    [스택,큐,덱] 9월 16일 - ing

---
+++ 9월10월-스택,큐,덱/20920.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;\
+
+bool cmp(string a, string b){
+
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<string> v;
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        string word;
+        cin>>word;
+
+        if(word.length()<m)
+            continue;
+
+        v.push_back(word);
+    }
+
+    sort(v.begin(), v.end(), cmp);
+}
