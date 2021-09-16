2a2c4845ae10d4f9feb301c7bde32d7e1736ef7f
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-16 23:29:37 +0900

    [스택,큐,덱] 9월 16일

---
+++ 9월10월-스택,큐,덱/10866.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(){
+    deque<int> dq;
+
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        string str;
+        cin>>str;
+
+        if(str=="push_front"){
+            int x;
+            cin>>x;
+            dq.push_front(x);
+        }
+        else if(str=="push_back"){
+            int x;
+            cin>>x;
+            dq.push_back(x);
+        }
+        else if(str=="pop_front"){
+            if(dq.empty())
+                cout<<"-1\n";
+            else {
+                cout<<dq.front()<<'\n';
+                dq.pop_front();
+            }
+        }
+        else if(str=="pop_back"){
+            if(dq.empty())
+                cout<<"-1\n";
+            else {
+                cout<<dq.back()<<'\n';
+                dq.pop_back();
+            }
+        }
+        else if(str=="size"){
+            cout<<dq.size()<<'\n';
+        }
+        else if(str=="empty")
+            cout<<dq.empty()<<'\n';
+        else if(str=="front"){
+            if(dq.empty())
+                cout<<"-1\n";
+            else
+                cout<<dq.front()<<'\n';
+        }
+        else{
+            if(dq.empty())
+                cout<<"-1\n";
+            else
+                cout<<dq.back()<<'\n';
+        }
+
+    }
+}
+
