250ee0ec6b0ed3483494ebacbd36ade780b55e81
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-20 22:23:43 +0900

    [우선순위 큐] 9월 20일 -Update

--- 9월14일-우선순위 큐/15903.cpp
+++ 9월14일-우선순위 큐/15903.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
+
 int main(){
     int n, m;
-    vector<long long> v; //수의 크기가 크므로 long long으로 선언하는 것이 좋다.
+    priority_queue <long long, vector<long long>, greater<>> pq; //수의 크기가 크므로 long long으로 선언
     long long answer=0;
 
     cin>>n>>m;
@@ -14,21 +15,24 @@ int main(){
     for(int i=0; i<n; i++){
         long long num;
         cin>> num;
-        v.push_back(num);
+        pq.push(num);
     }
-
-    sort(v.begin(), v.end()); //오름차순 정렬
+    //우선순위 큐이므로 자동으로 오름차순 정렬 - greater 썼으므로
 
     for(int i=0; i<m; i++){
-        long long r = v[0]+v[1]; //가장 작은 것 2개 뽑아서 더하기
-        v[0]=r;//더한 값 저장하기
-        v[1]=r;//더한 값 저장하기
+        long long a = pq.top(); //가장 작은 것 2개 뽑아서 더하기
+        pq.pop();
+        long long b = pq.top();
+        pq.pop();
+        long long r = a+b;
+        pq.push(r);//더한 값 저장하기
+        pq.push(r);//더한 값 저장하기
 
-        sort(v.begin(), v.end()); //다시 정렬
     }
 
     for(int i=0; i<n; i++){
-        answer += v[i];
+        answer += pq.top();
+        pq.pop();
     }
 
     cout<<answer;
