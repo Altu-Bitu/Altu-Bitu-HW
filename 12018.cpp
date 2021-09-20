8f3d24b06955fc5fedc8d6a3fa1cac35d77bf650
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-20 11:04:52 +0900

    [우선순위 큐] 9월 20일

--- 12018.cpp
+++ 12018.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 int main(){
-    vector<int> v;
+    vector<int> v; //해당 과목 수강하기 위해 필요한 최소 마일리지 저장할 벡터
 
     int test;
     int mileage;
+    int cnt=0;
 
     cin>>test>>mileage;
 
@@ -20,26 +21,29 @@ int main(){
            cin>>p[j];
         }
 
-        //make_heap(p.begin(), p.end(), greater<int>());
         sort(p.begin(), p.end(), greater<int>());
 
-        if(peo<limit){
-            v.push_back(1);
+        if(peo<limit){ //신청한 사람 수 < 수강신청 가능 사람 수
+            v.push_back(1); //마일리지 1이면 수강신청 가능
         }
-        else{
-            v.push_back(p[limit-1]);
+        else{ //신청한 사람 수 >= 수강신청 가능 사람 수
+            v.push_back(p[limit-1]); //limit 번째 마일리지면 수강신청 가능
         }
 
     }
-    sort(v.begin(), v.end());
 
-    int idx=0;
-    while(mileage-v[idx]>=0){
-        mileage -= v[idx];
-        idx++;
+    sort(v.begin(), v.end()); //오름차순 정렬, 작은 것부터 빼서 최대로 수강신청 가능하게 하도록 한다.
+
+    for(int i=0; i<v.size(); i++){
+        if(mileage - v[i]>=0){
+            mileage -= v[i];
+            cnt++;
+        }
+        else
+            break;
     }
 
-    cout<<idx;
+    cout<<cnt;
 
 }
 
