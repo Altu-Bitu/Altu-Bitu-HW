7676f43b316f5ac37f68eb16a1f8ea0b47d3eee0
Kyoungwon Min <mkkw2047@gmail.com>
2021-09-28 16:27:35 +0900

    [브루트포스] 9월 28일 -Update

--- 9월 21일-브루트 포스/10448.cpp
+++ 9월 21일-브루트 포스/10448.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+void makeTriangularNum(vector<int> v){//삼각수 만들어서 벡터에 저장
+    for (int i = 1; i <= 44; i++) {
+        v.push_back(i * (i + 1) / 2);
+    }
+}
+
 bool triangularSum(vector<int> v, int num) //모든 경우의 삼각수 합을 계산
 {
     for (int i = 0; i < v.size(); i++) {
@@ -21,20 +27,14 @@ bool triangularSum(vector<int> v, int num) //모든 경우의 삼각수 합을 계산
 int main() {
     vector<int> v;
     int t, k;
-
     cin >> t;
 
-    for (int i = 1; i <= 44; i++) {
-        v.push_back(i * (i + 1) / 2);
-    }
-
+    makeTriangularNum(v); //삼각수를 만들어서 저장
     for (int i = 0; i < t; i++) {
         cin >> k;
 
         bool result = triangularSum(v, k);
-        if (result)
-            cout << "1\n";
-        else
-            cout << "0\n";
+        cout<<result<<'\n';
+        //result값이 true이면 1로, false이면 0으로 출력됨. 숫자로 바꿀 필요 없음.
     }
 }
