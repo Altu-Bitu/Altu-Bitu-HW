9f7c6b5159bb61ce0d712c3f559146d6b7ef08d1
Kyoungwon Min <mkkw2047@gmail.com>
2021-10-12 18:42:19 +0900

    [분할정복] 10월 12일 -Update

--- 10월 5일 분할 정복/16198.cpp
+++ 10월 5일 분할 정복/16198.cpp
@@ -1,65 +1,94 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//구슬 3개씩 묶기
-//가운데 기준 양 옆 구슬 에너지의 곱이 최대인 것 찾기
-//최대인 에너지의 곱이 같다면 가운데 구슬의 에너지가 가장 작은 것 찾기
-bool cmp(const pair<int, int> &p1, const pair<int, int> &p2){
-    if(p1.first==p2.first) { //에너지 곱한 값이 같은 경우 가운데 값이 작은 것이 먼저 오도록
-        return p1.second < p2.second;
+vector<int> w;
+int ans;
+
+void backtracking(int sum) { //백트래킹 이용 - 가지치기, 인자:sum
+    if (w.size() == 2) { //에너지 구슬이 2개 -> 더 이상 고를 수 없음
+        ans = max(ans, sum); //최댓값을 답으로
+        return;
     }
-    return p1.first>p2.first; //에너지 곱한 값 기준 내림차순
+    for (int i = 1; i < w.size() - 1; i++) { //처음과 마지막 선택 불가이므로
+        int temp_w = w[i]; //현재 구슬
+        int sum_w = w[i - 1] * w[i + 1]; //현재 에너지
+        w.erase(w.begin() + i); //문제에 따라 현재 구슬을 없앰.
+        backtracking(sum + sum_w); //각 경우마다 에너지 값을 모두 구해서 비교하자.
+        w.insert(w.begin() + i, temp_w); //위의 backtracking에서 return한 경우 다른 경우로 넘어가서 비교해야하므로 erase 했던 값 insert하기
+    }
 }
 
-int main(){
-    vector<int>v;
-    vector<pair<int, int>> e; //에너지 곱한 값, x값
+/**
+ * 가능한 모든 경우를 구해서 계산하자
+ * 이때, 사용한 에너지는 따로 저장해 둔 후에 배열에서 지우고, 다시 돌아왔을 때 저장한 값을 배열에 넣는다.
+ */
+int main() {
     int n;
-    cin>>n;
-    int answer=0;
 
-    for(int i=0; i<n; i++){
-        int w;
-        cin>>w;
-        v.push_back(w);
-    }
+    //입력
+    cin >> n;
+    w.assign(n, 0);
+    for (int i = 0; i < n; i++)
+        cin >> w[i];
 
-    //반례:
-    /* 5
-     * 3 1 2 4 5
-     * 정답: 33, 결과: 31
-     * 1->2->3 순서로 공을 제거해야하는데 내 코드에서는 4->1->2 이렇게 제거한다
-     * e에서 가장 앞의 값을 지우는 것이 아니라 e에 있는 모든 경우를 다 해봐야하는 것일까?
-     */
-    while(v.size()>=3) {
-        for (int i = 1; i < v.size() - 1; i++) {
-            e.emplace_back(make_pair(v[i - 1] * v[i + 1], v[i]));
-            //pair로 만들어서 e에 저장
-        }
+    //연산
+    backtracking(0); //모든 경우 비교해서 최댓값 찾기
+/* 7
+ * 2 2 7 6 90 5 9
+ * i=1
+ * temp_w = w[1] = 2
+ * sum_w = w[0]*w[2]=14
+ * erase: 2 7 6 90 5 9
+ * backtracking(0+2);
+ *  i=1
+ *  temp_w = w[1] = 7
+ *  sum_w = 12
+ *  w: 2 6 90 5 9
+ *  backtracking(2+12)
+ *      i=1
+ *      temp_w = w[1] = 6
+ *      sum_w = 180
+ *      w: 2 90 5 9
+ *      backtracking(14+180)
+ *          i=1
+ *          temp_w = w[1] = 90
+ *          sum_w = 10
+ *          w: 2 5 9
+ *          backtracking(194+10)
+ *              i=1
+ *              temp_w = w[1] = 5
+ *              sum_w = 18
+ *              w: 2 9
+ *              backtracking(204+18)
+ *                  ans = 222
+ *                  return;
+ *              w: 2 5 9
+ *          w: 2 90 5 9
+ *          i=2
+ *          temp_w = w[2] = 5
+ *          sum_w = 90*9 = 810
+ *          w: 2 90 9
+ *          backtracking(194 + 810)
+ *              i=1
+ *              temp_w = w[1] = 90
+ *              sum_w = 18
+ *              w: 2 9
+ *              backtracking(1004+18)
+ *              ans = 1022
+ *              return;
+ *      i=2
+ *      temp_w = w[2] = 90
+ *      sum_w = 30
+ *      w: 2 6 5 9
+ *      ...
+ *
+ *
+ *
+ */
+    //출력
+    cout << ans << '\n';
 
-        sort(e.begin(), e.end(), cmp); //e정렬
-
-        //for (int i = 0; i < e.size(); i++) {
-        //    cout << e[i].first << ' ' << e[i].second << '\n';
-        //}
-
-        for (int i = 1; i < v.size() - 1; i++) {
-            if (v[i] == e[0].second) { //e에서 가장 앞에 있는 값을 v에서 삭제한다.
-                v.erase(v.begin() + i);
-                //cout<<"vsize: "<<v.size()<<'\n';
-                break;
-            }
-        }
-
-        answer += e[0].first;
-
-        e.clear();
-    }
-
-    cout<<answer;
-
+    return 0;
 }
-
