9ab9caa814854a2ad5534268c8a4b40268d687bc
Kyoungwon Min <mkkw2047@gmail.com>
2021-12-06 22:36:09 +0900

    [최소 신장 트리] 12월 4일 -Update

--- 11월 30일-최소 신장 트리/1713.cpp
+++ 11월 30일-최소 신장 트리/1713.cpp
@@ -4,53 +4,68 @@
 using namespace std;
 
 typedef pair<int, int> ci;
-map<int, ci> stu; //학생, 추천횟수, 들어간 시점 - 학생으로 바로 찾기 위해 map 사용
+map<int, ci> stu; //사진틀 - 학생, 추천횟수, 들어간 시점 - 학생으로 바로 찾기 위해 map 사용
 
-int main(){
-    int n, m;
-    cin>>n>>m;
+//사진틀이 꽉 찬 경우 - 제거
+int deleteStu(){
+    pair<int, int> pr; //map의 처음 것 정보 저장
+    int idx = stu.begin()->first; //처음 - 학생
+    pr = stu.begin()->second; //처음 - 추천횟수, 들어간 시점 
 
+    for(auto item : stu){ //map 순회
+        if(item.second.first < pr.first){ //1. 추천횟수가 적은 것 삭제 대상
+            idx = item.first;
+            pr = item.second;
+        }
+        if(item.second.first == pr.first){
+            if(item.second.second<pr.second){ //2. 추천횟수가 같다면 들어간 시점이 작은 것 삭제 대상
+                idx = item.first;
+                pr = item.second;
+                    
+            }
+        }
+    }
+    return idx;
+}
+
+//사진틀에 추천한 학생이 없는 경우 - 학생 넣기
+void insertStu(int n, int num, int time){
+    if(stu.size()<n){ //현재 사진틀 개수가 n보다 작은 경우 그냥 추가- 정상
+        stu[num].first=1; //추천 횟수
+        stu[num].second=time; //들어간 시점
+    }
+    else{ //예외 - 사진틀이 꽉 차서 기존의 것 지워야 되는 경우
+        int idx = deleteStu();
+        stu.erase(idx); //1,2 조건에 따라 map에서 idx 삭제
+        //num 정보 추가
+        stu[num].first=1;
+        stu[num].second=time;
+    }
+}
+
+//학생 추천
+void recomStu(int n, int m){
     for(int i=0; i<m; i++){
         int num;
         cin>>num;
 
         if(stu.find(num)==stu.end()){ //stu에 num학생이 없는 경우 - 추가해야함
-
-            if(stu.size()<n){ //현재 사진틀 개수가 n보다 작은 경우 그냥 추가- 정상
-                stu[num].first=1; //추천 횟수
-                stu[num].second=i; //들어간 시점
-            }
-            else{ //예외 - 기존의 것 지워야 되는 경우
-                pair<int, int> pr; //map의 처음 것 정보 저장
-                int idx = stu.begin()->first; //처음 - 학생
-                pr = stu.begin()->second; //처음 - 추천횟수, 들어간 시점 
-
-                for(auto item : stu){ //map 순회
-                    if(item.second.first < pr.first){ //1. 추천횟수가 적은 것 삭제 대상
-                        idx = item.first;
-                        pr = item.second;
-                    }
-                    if(item.second.first == pr.first){
-                        if(item.second.second<pr.second){ //2. 추천횟수가 같다면 들어간 시점이 작은 것 삭제 대상
-                            idx = item.first;
-                            pr = item.second;
-                        }
-                    }
-                }
-
-                stu.erase(idx); //1,2 조건에 따라 map에서 idx 삭제
-                //num 정보 추가
-                stu[num].first=1;
-                stu[num].second=i;
-
-            }
+            insertStu(n, num, i);           
         }
         else{//map에 있다면 추천횟수만 증가
             stu[num].first++;
         }
 
     }
+}
 
+int main(){
+    int n, m;
+    cin>>n>>m;
+
+    recomStu(n,m); //학생 추천
+
+    //사진틀에 있는 학생 출력
     for(auto item: stu){
         cout<<item.first<<' ';
     }
