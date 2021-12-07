dc60d1c1a86f0d3db658cec78e8a3221b7fb9455
Kyoungwon Min <mkkw2047@gmail.com>
2021-12-07 14:05:42 +0900

    [최소 신장 트리] 12월 7일 -Update

--- 11월 30일-최소 신장 트리/1713.cpp
+++ 11월 30일-최소 신장 트리/1713.cpp
@@ -1,75 +1,54 @@
 #include <iostream>
+#include <vector>
 #include <map>
 
 using namespace std;
-
 typedef pair<int, int> ci;
-map<int, ci> stu; //사진틀 - 학생, 추천횟수, 들어간 시점 - 학생으로 바로 찾기 위해 map 사용
 
-//사진틀이 꽉 찬 경우 - 제거
-int deleteStu(){
-    pair<int, int> pr; //map의 처음 것 정보 저장
-    int idx = stu.begin()->first; //처음 - 학생
-    pr = stu.begin()->second; //처음 - 추천횟수, 들어간 시점 
-
-    for(auto item : stu){ //map 순회
-        if(item.second.first < pr.first){ //1. 추천횟수가 적은 것 삭제 대상
-            idx = item.first;
-            pr = item.second;
-        }
-        if(item.second.first == pr.first){
-            if(item.second.second<pr.second){ //2. 추천횟수가 같다면 들어간 시점이 작은 것 삭제 대상
-                idx = item.first;
-                pr = item.second;
-                    
-            }
+map<int, ci>::iterator delCandidate(map<int, ci> &candidate) { //사진틀이 꽉 차있을 때, 지울 학생 선택하기
+    auto del = candidate.begin(); //처음 후보를 삭제한다 가정
+    int cnt = candidate.begin()->second.first; //처음 후보의 추천 횟수
+    int t = candidate.begin()->second.second; //처음 후보의 게시 시간
+    for (auto iter = ++candidate.begin(); iter != candidate.end(); iter++) { //map 순회
+        int cur_cnt = iter->second.first; //현재 학생의 추천횟수
+        int cur_t = iter->second.second; //현재 학생이 사진틀에 들어간 시점
+        if (cur_cnt < cnt) { //추천 횟수가 가장 작은 후보 찾기
+            cnt = cur_cnt; //추천횟수를 현재 값으로 갱신
+            t = cur_t; //저장시점을 현재 값으로 갱신
+            del = iter; //후보를 현재 후보로 갱신
+        } else if (cur_cnt == cnt && cur_t < t) { //추천 횟수가 가장 작은 후보가 여러명이라면, 게시 시간이 오래된 후보 찾기
+            t = cur_t; //저장시점을 현재 값으로 갱신
+            del = iter; //후보를 현재 후보로 갱신
         }
     }
-    return idx;
+    return del; //최종적으로 선정된 학생을 리턴 (삭제할 후보)
 }
 
-//사진틀에 추천한 학생이 없는 경우 - 학생 넣기
-void insertStu(int n, int num, int time){
-    if(stu.size()<n){ //현재 사진틀 개수가 n보다 작은 경우 그냥 추가- 정상
-        stu[num].first=1; //추천 횟수
-        stu[num].second=time; //들어간 시점
+/**
+ * 1. 비어있는 사진틀이 없는 경우, 가장 추천수가 작은 학생 중 게시 시간이 오래된 학생을 삭제
+ * 2. 후보 학생을 바로 찾기 위해 본 풀이는 map 컨테이너를 사용해 구현
+ *
+ * !주의! 게시 시간 정보 저장 시, 후보로 올라간 가장 첫 시간을 저장. 이미 후보에 있는데 게시 시간이 갱신되지 않도록 주의.
+ */
+
+int main() {
+    int n, m, input;
+
+    //입력 & 연산
+    cin >> n >> m;
+    map<int, ci> candidate; //first: 후보 학생, second: {추천 횟수, 게시 시간}
+    for (int i = 0; i < m; i++) { //m번 추천 받음
+        cin >> input; //추천할 학생 번호 입력 받음
+        if (candidate.size() == n && candidate.find(input) == candidate.end()) //비어있는 사진틀이 없고 추천받은 학생이 사진틀에 없는 경우
+            candidate.erase(delCandidate(candidate)); //사진틀에서 삭제
+
+        if (candidate.find(input) == candidate.end()) //첫 게시라면 == 사진틀에 없는 학생이라면
+            candidate[input].second = i; //들어간 시점 저장
+
+        candidate[input].first++; //사진틀에 있는 학생이라면 추천 횟수 증가
     }
-    else{ //예외 - 사진틀이 꽉 차서 기존의 것 지워야 되는 경우
-        int idx = deleteStu();
-        stu.erase(idx); //1,2 조건에 따라 map에서 idx 삭제
-        //num 정보 추가
-        stu[num].first=1;
-        stu[num].second=time;
-    }
+
+    //출력
+    for (auto iter = candidate.begin(); iter != candidate.end(); iter++)
+        cout << iter->first << ' '; //map 순회하면서 학생 번호 출력
 }
-
-//학생 추천
-void recomStu(int n, int m){
-    for(int i=0; i<m; i++){
-        int num;
-        cin>>num;
-
-        if(stu.find(num)==stu.end()){ //stu에 num학생이 없는 경우 - 추가해야함
-            insertStu(n, num, i);           
-        }
-        else{//map에 있다면 추천횟수만 증가
-            stu[num].first++;
-        }
-
-    }
-}
-
-int main(){
-    int n, m;
-    cin>>n>>m;
-
-    recomStu(n,m); //학생 추천
-
-    //사진틀에 있는 학생 출력
-    for(auto item: stu){
-        cout<<item.first<<' ';
-    }
-}
-
-//참고: https://steady-life.tistory.com/m/61
-//학생 번호를 바로바로 사용하면 효율적일 것 같아 map을 쓰기로 결정했지만 구현을 하기가 어려워 도움을 받았습니다.
