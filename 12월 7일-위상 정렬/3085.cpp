6e3d2b8dda926aabda566fd7b24a51db046b3efe
Kyoungwon Min <mkkw2047@gmail.com>
2021-12-20 17:19:08 +0900

    [위상 정렬] 12월 20일 -Update

--- 12월 7일-위상 정렬/3085.cpp
+++ 12월 7일-위상 정렬/3085.cpp
@@ -14,37 +14,43 @@ bool valid(int n, int a, int b){ //인접 범위 체크
     return true;
 }
 
-int countCandy(vector<vector<char>> &v, int n){
+int countCandy(vector<vector<char>> &v, int n){ //연속된 사탕 개수 세는 함수
     int cnt=1;
-    for(int j=0; j<n; j++){ //열
+    for(int j=0; j<n; j++){ //열 - 연속된 사탕 개수 세기
         int ccnt=1;
         for(int i=0; i<n-1; i++){
             if(v[i][j]==v[i+1][j]){
                 ccnt++;
             }
-        }
-        cnt = max(cnt, ccnt);
+            else{
+                ccnt=1;
+            }
+            cnt = max(cnt, ccnt);
+        }        
     }
-    for(int i=0; i<n; i++){ //행
+    for(int i=0; i<n; i++){ //행 - 연속된 사탕 개수 세기
         int rcnt=1;
         for(int j=0; j<n-1; j++){
             if(v[i][j]==v[i][j+1]){
                 rcnt++;
             }
-        }
-        cnt = max(cnt, rcnt);
+            else{
+                rcnt=1;
+            }
+            cnt = max(cnt, rcnt);
+        }        
     }
-
     return cnt;
 }
 
-int maxCandy(int n){
+int maxCandy(int n){ // 최대 연속 사탕 개수 세기
     vector<vector<char>> cboard(50, vector<char>(50,' '));
-    copy(board.begin(), board.end(), cboard.begin()); //벡터 복사
-    int res=countCandy(cboard,n);
+    copy(board.begin(), board.end(), cboard.begin()); //벡터 복사(board->cboard로 복사)
+    int res=countCandy(cboard,n); //처음 board의 최대 연속 사탕 개수 세기
+    
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            for(int t=0; t<4; t++){
+            for(int t=0; t<4; t++){ //인접 좌표의 사탕 색깔이 다른 경우 swap하고 개수 세고 다시 swap해서 원래대로 돌려놓기
                 int x = i + dx[t];
                 int y = j + dy[t];
                 if(valid(n,x,y)){
@@ -65,6 +71,7 @@ int main(){
     int n;
     int answer;
 
+    //입력
     cin>>n;
     
     for(int i=0; i<n; i++){
@@ -72,64 +79,10 @@ int main(){
             cin>>board[i][j];
         }
     }
+
+    //연산
     answer = maxCandy(n);
+    
+    //출력
     cout<<answer;    
 }
-
-/*
-반례
-
-6
-CCYYCC
-YYCCYY
-CCYYCC
-YYCCYY
-CCYYCC
-YYCCYY
-답: 3
-- 2로 나옴
-
-40
-YYYYYYYYYCYYYYYYYYYYYYYYYCZZZZZZZYYYYYZC
-PYPCYZCPYCZZCCPZYYPZYYYYPPZZPYCCCZYZZZPY
-ZPPCYCCPYYZYPPZCZPYCCCZZYYPZZPYPPPPZPCZC
-ZCYYZZYCPPPCCYPYYPZZZZCCCCZZCZCYCYZCZZYC
-PYZYZZCCZZCCPPYCZPYPPZYZYYYZZPPCCZYYYZCZ
-PPPCZZPCCCCCCCCCCCCCCCCCCYCZYYYZCYYCPCPZ
-PZYPZYPPYYYZZZPPPZPYZPYZCZZPYZCZZPPCYCCZ
-YPPYYYYCCPYPZPCPPPCZCZZYZZZZYYZPZZCCCZYY
-YPZCZCPZYYZCCPPPYPPCCPCYZYYYCPPPYCYYCPYC
-ZPYZCCZCYZYYCPCCPPYYZPYCCPPCPZCCZCCZYYPY
-CPPPCPZZZCYCPYCZYZZPYPZCYYCCZCZZPZYCPZCZ
-YZYCYPCPPYPPPPYYYPYPCPCPZZPPCYZCZPZZZZYP
-ZCZPYZPPYYYPYPCZYZZYZZPZCZPPPZYCZYPCPYYC
-YPZPZYCCYPZZCCPYYCYZYYYYYCZYZZYZZPPYCZCZ
-YCZPZCCCCCYCCCCCCCCCCCZYPYZPCZPZZPZZYPYY
-YYYYYYYYYYPYYYYYYYYYYYYYYYYYYYYYYYYYYZZP
-ZYZCZZCCZPZCZYCPYPCCPYZYCCPPZZCZYCZCYPYP
-YYZYPZZYCPCYCZPZYCZPZCCZYCCCZZZYYYZYYPCP
-YZZPZYPYCZCPPCZPYCCPYCYZPCCYYZYPCPPPYYPZ
-YYZPPZYCZZZYYPYCYZCCYYZPYCCYPZCCZCCCYYZC
-CYZZPPCZYZYCCPCYYCPZPPZZPCZZYZZYZZCZYYPC
-PPZYZYZPZZZZYPZYYPZPPZPPZCYCPZYZPPYYYYYZ
-CZZZYPZYCCYYYPPZYCYPZPCCPCYYYZZYCPYCYCYY
-YCZZCZCCYPPYYZYYYPPPZZYYCCCYYZZZYZZZYYCC
-YCZPZPPPZPCYYYZZYCPPZYPZYCZZZZZPYYPYCYPC
-PCZZZYYZCPCPCZYYYCPYZCCPZCZPYZZPYPZPYZYY
-ZZZZPPPPPYZCZCPYYCCCCCCCCCCCYCCCCCCPCCCP
-CPYCYZCZCZCYCPCYYCYZCZYYZCCPZZYZPZCPYCCP
-YZZYYZZZZPZZCZCYYCZZPYZYCCPCPZYCYCZPYZPZ
-YZZZZZZZZZZZZZZZZZZZZZZZCZZZZZZZZZZZZZZZ
-YZZPPPPPPPPPPPPPPCPPPPPYYCYCZZZCCPCCYPYZ
-YYZCPYPPYZPPYCZPYCZPCPCZZZCYCZYZCPCPZPZZ
-CYZPCYYYYYYYYYYYYCYYYYYYYYYYYZYYCYZYPYZC
-CPZCCZZZZZZZZZZZZCZZZZZZZZZZYZCCZCPZZCCY
-YYZPCPZZCYYYYYCPYCZPYYYPPZZCCZZCPPPPCCPP
-YYZZPCCYZCCCYPCYYPZCZZZZZPPYZCCCCCZCPPCY
-PYYYYYYYYYYYYYYYYYYYYYYPYCZZPPYZYPPPPYCC
-YPZCZPZYPZPPCYCZYCYPCCCZCZZCCZYZYYCYYZCZ
-CPPZZPCYYCCCYCCPZPYYZYCYZYZYPYCPPZPCPCYC
-ZPCYPYZPPCYYPYZZZPPZZZCCPYCYPCYYCYPPYCZY
-답: 37
--38로 나옴
-*/
